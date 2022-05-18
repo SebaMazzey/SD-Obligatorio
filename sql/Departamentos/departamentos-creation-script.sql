bbbe3ea9e885411e6d2b392e9df0f13e858d2169
NicolasPuig <nicopuig14@hotmail.com>
2022-05-18 01:21:43 -0300

    Fix sql script

--- sql/Departamentos/departamentos-creation-script.sql
+++ sql/Departamentos/departamentos-creation-script.sql
@@ -1,11 +1,11 @@
 -- Script de creación de base de datos para cada departamento
-CREATE DATABASE IF NOT EXISTS `Departamentos`;
-USE `Departamentos`;
+CREATE DATABASE IF NOT EXISTS `Departments`;
+USE `Departments`;
 
 CREATE TABLE `Circuits` (
   `Number` int NOT NULL,
   PRIMARY KEY (`Number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `EnabledDevices` (
   `Code` varchar(767) NOT NULL,
@@ -13,18 +13,18 @@ CREATE TABLE `EnabledDevices` (
   PRIMARY KEY (`Code`),
   KEY `IX_EnabledDevices_Circuit_Number` (`Circuit_Number`),
   CONSTRAINT `FK_EnabledDevices_Circuits_Circuit_Number` FOREIGN KEY (`Circuit_Number`) REFERENCES `Circuits` (`Number`) ON DELETE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `Options` (
   `Name` varchar(767) NOT NULL,
   PRIMARY KEY (`Name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `Users` (
   `CI` varchar(64) NOT NULL,
-  `AlreadyVoted` BOOLEAN NOT NULL,
+  `Already_Voted` BOOLEAN NOT NULL,
   PRIMARY KEY (`CI`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `Votes` (
   `Id` int NOT NULL AUTO_INCREMENT,
@@ -35,5 +35,5 @@ CREATE TABLE `Votes` (
   KEY `IX_Votes_Option_Name` (`Option_Name`),
   CONSTRAINT `FK_Votes_Circuits_Circuit_Number` FOREIGN KEY (`Circuit_Number`) REFERENCES `Circuits` (`Number`) ON DELETE RESTRICT,
   CONSTRAINT `FK_Votes_Options_Option_Name` FOREIGN KEY (`Option_Name`) REFERENCES `Options` (`Name`) ON DELETE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
