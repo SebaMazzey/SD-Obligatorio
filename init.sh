588dfd349dfcc3dc2f64c9491f3d74262ff297d2
NicolasPuig <nicopuig14@hotmail.com>
2022-05-29 10:24:14 -0300

    Configuracion de containers

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
+echo "Compilando Servidor Central..."
+dotnet publish ./API-Servidor-Central/Central-API.sln -c Release
+echo "Servidor Central compilado con exito"
+echo "Compilando Servidor Departamental..."
+dotnet publish ./API-Servidor-Departamento/Departments-API.sln -c Release
+echo "Servidor Departamental compilado con exito"
 echo "Iniciando Sistema..."
-docker-compose build --no-cache
-docker-compose up
+docker-compose build --no-cache 
+docker-compose up --remove-orphans
