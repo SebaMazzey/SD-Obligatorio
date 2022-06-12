55774a12c6c482b0eb3b44ecb1972c0a6c510dcb
Seba Mazzey <mazzeyseba@gmail.com>
2022-06-11 21:20:04 -0300

    Departamental app with login screen + entral server blank form

---
+++ API-Servidor-Departamento/DepartamentoApp/AppVotos.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+using System.Globalization;
+
+namespace DepartamentoApp
+{
+    partial class AppVotos
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.user_ci = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(307, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "C.I.";
+            // 
+            // user_ci
+            // 
+            this.user_ci.Location = new System.Drawing.Point(337, 212);
+            this.user_ci.MaxLength = 11;
+            this.user_ci.Name = "user_ci";
+            this.user_ci.PlaceholderText = "1.234.567-8";
+            this.user_ci.Size = new System.Drawing.Size(100, 23);
+            this.user_ci.TabIndex = 1;
+            this.user_ci.TextChanged += new System.EventHandler(this.UserCI_TextChanged);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(352, 253);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "Ingresar";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // AppVotos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.user_ci);
+            this.Controls.Add(this.label1);
+            this.Name = "AppVotos";
+            this.Text = "App Votación";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox user_ci;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
+
