e320e9eaf783ee085eb2c6cc5bd720e2729ae231
Seba Mazzey <mazzeyseba@gmail.com>
2022-06-18 17:41:17 -0300

    Circuit voting app

--- Circuitos/CircuitosApp/Program.cs
+++ Circuitos/CircuitosApp/Program.cs
@@ -1,3 +1,4 @@
+using CircuitosApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,12 @@ namespace DepartamentoApp
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AppVotos());
+
+            // Setup baseUrl and circuitNumber
+            HttpService.AddBaseUrl("http://localhost:8001");
+            var circuitNumber = 0;
+
+            Application.Run(new AppVotos(circuitNumber));
         }
     }
 }
---
+++ Circuitos/CircuitosApp/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitosApp.Services
+{
+    public class UserService
+    {
+        public string SessionToken { get; private set; }
+
+        public UserService()
+        {
+        }
+
+        /// <summary>
+        /// Intenta loguear al usuario en la api departamental para proceder con la votación.
+        /// </summary>
+        /// <param name="userCi">CI del usuario</param>
+        /// <return> Devuelve true si el usuario se loguea con exito.</returns>
+        public bool LogInUser(string userCi)
+        {
+            var resultToken = HttpService.CallDepartmentApiAsync("User/Verify?ci=" + userCi, HttpService.RequestType.Get);
+            if (resultToken != "")
+            {
+                this.SessionToken = resultToken;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Limpia el token en caso de que el procesos de votación finalice con exito
+        /// </summary>
+        public void ClearToken()
+        {
+            this.SessionToken = "";
+        }
+    }
+}
